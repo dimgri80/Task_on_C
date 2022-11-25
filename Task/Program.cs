8dd6e2c17a4e7786a7875d5ef94a9f6bcee1398c
dimgri80 <dimgri80@gmail.com>
2022-11-25 22:47:06 +0300

    Task done

---
+++ Task/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+
+//int n= int.Parse(Console.ReadLine());
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write(n % 10);
+
+
+
+//int i = n *(-1);
+
+//while (i <= n)
+//  {
+    
+//    Console.Write(i + " "); 
+  
+//    i++;
+  
+//  }
+
+//for (int i = (-1) * n; i <= n; i++)
+    //Console.Write(i + " ");
+
+
+//Console.Write(n*2);
