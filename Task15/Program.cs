83986cfe0c8ba6fc300b348d486cc7a0af9dc23c
dimgri80 <dimgri80@gmail.com>
2022-11-30 23:17:50 +0300

    Hometask done

---
+++ Task15/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+
+while (true)
+
+{
+
+Console.Write ("Введите число N: ");
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+string t="Не является днем недели";
+
+int i = 1;
+
+for (i = 1; i <= 7; i++)
+
+{
+
+if (n == i && n < 6  )     {t="Нет";}
+
+if (n == i && n > 5  )     {t="Да";}
+
+}
+
+Console.WriteLine(t);
+
+}
