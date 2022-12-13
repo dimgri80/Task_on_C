c2e2958e4b33774a53be468d1c29d8c635db4892
dimgri80 <dimgri80@gmail.com>
2022-12-13 22:51:19 +0300

    Hometask done

---
+++ Task41/Program.cs
@@ -0,0 +1,69 @@
+﻿
+
+Console.Clear();
+
+while (true)
+
+{
+
+int i=0;
+
+Console.Write("Введите количество элементов массива: ");
+
+int m = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[m];
+
+void InputArray(int[] array)
+{
+
+for (i=0;i<m;i++)
+{
+Console.Write("Введите " +i+" элемент массива: ");
+
+array[i]  = Convert.ToInt32(Console.ReadLine());
+
+}
+
+
+for (i=0;i<m;i++)
+{
+    
+if (i==0)
+
+{Console.Write("["+array [i]+", ");}
+
+if (i!=0 && i!=m-1)
+
+{Console.Write(array [i]+", ");}
+
+if (i==m-1)
+
+{Console.WriteLine(array [i]+"]");}
+
+}
+}
+
+void ReleaseArray(int[] array)
+
+{
+
+int k=0;
+
+for (i=0; i<m; i++)
+
+{
+
+if (array[i]>0) {k++;}
+
+
+}  
+
+Console.WriteLine("В массиве "+ k +" элемента больше ноля.");
+}
+
+InputArray(array);
+
+ReleaseArray(array);
+
+}
---
+++ Task43/Program.cs
@@ -0,0 +1,39 @@
+﻿
+
+Console.Clear();
+
+while (true)
+
+{
+
+double x = 0;
+
+Console.Write("Введите k1: ");
+
+double k1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите b1: ");
+
+double b1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите k2: ");
+
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите b2: ");
+
+double b2 = Convert.ToInt32(Console.ReadLine());
+
+while ((k1 * x + b1 != k2 * x + b2) ) 
+
+{   
+  
+    if (k1 * -x + b1 == k2 * -x + b2) {x=-x-0.1;}
+
+    x=x+0.1;
+    
+}
+
+Console.WriteLine("Точка пересечения прямых имеет координаты ("+x+","+(k1 * x + b1)+").");
+
+}
