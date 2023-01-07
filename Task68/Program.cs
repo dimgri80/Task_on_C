e0346fd017d7700f7fbfa303b4f5877cbbc621e8
dimgri80 <dimgri80@gmail.com>
2023-01-07 22:33:49 +0300

    Hometask done

---
+++ Task68/Program.cs
@@ -0,0 +1,29 @@
+﻿int ack (int a, int b)
+
+{
+
+    if (a == 0)
+
+        return b+1;
+
+    else if ((a!=0)&&(b==0))
+
+        return ack(a-1,1);
+
+    else
+
+        return ack(a-1,ack(a,b-1));
+
+}
+
+Console.Clear();
+
+Console.Write("Введите значение M: ");
+
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите значение N: ");
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Функция Аккермана для чисел "+ m +" и "+ n +" равна: " + ack (m , n));
