5694715a50693e8a5804c462b9e3036e9a94a5a7
dimgri80 <dimgri80@gmail.com>
2023-01-07 22:34:49 +0300

    Hometask done

---
+++ Task66/Program.cs
@@ -0,0 +1,23 @@
+﻿int rec (int a, int b)
+
+{
+
+    if (a == b)
+
+        return b;
+
+    return rec(a, b - 1) + b;
+
+}
+
+Console.Clear();
+
+Console.Write("Введите значение M: ");
+
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите значение N: ");
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Сумма натуральных чисел между "+ m +" и "+ n +" равна: " + rec (m , n));
